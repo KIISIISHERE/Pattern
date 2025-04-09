a9169ff0ece85355c23bd7d41a3a6d3c1fea7e77
KIISIISHERE <iteoluwakiisi420@gmail.com>
2025-04-09 17:49:34 +0000

    I made another code

---
+++ main1.py
@@ -0,0 +1,11 @@
+# Take input
+n = int(input("Enter the number of rows: "))
+# Outer loop to handle number of rows
+for i in range(n):
+    # Inner loop to handle number of columns
+    for j in range(i + 1):
+        # Display Result
+        print("* ", end="")
+        # Newline after each riw
+    print()
+    
